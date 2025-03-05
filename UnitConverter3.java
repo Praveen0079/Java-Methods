da39362c77b6a4896fa9a3a0d13f4b559db62b53
Praveen Sharma <96952903+Praveen0079@users.noreply.github.com>
2025-03-05 23:06:23 +0530

    Unit Converter

--- UnitConverter3.java
+++ UnitConverter3.java
@@ -4,7 +4,7 @@ public class UnitConverter3 {
     public static double convertFahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
     }
-
+ 
     // Convert Celsius to Fahrenheit
     public static double convertCelsiusToFahrenheit(double celsius) {
         return (celsius * 9 / 5) + 32;
