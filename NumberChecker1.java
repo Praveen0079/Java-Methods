929772cfab68f2100691ad25e665dd8471d52813
Praveen Sharma <piyush20112@gmail.com>
2025-03-06 11:24:55 +0530

    Count , Sum , duck , armStrong

--- NumberChecker1.java
+++ NumberChecker1.java
@@ -1,11 +1,17 @@
-import java.util.Scanner;
+import java.util.*;
 
 class NumberChecker1{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		System.out.println("Number of digits : "+count(n));
-		System.out.println("Sum of digits : "+SumOfDigits(n));
+		int n = sc.nextInt(); //132
+		System.out.println("Number of digits : "+count(n)); //3
+		System.out.println("Sum of digits : "+SumOfDigits(n)); //6
+		if(isDuckNumber(n)){
+			System.out.println(n+" is a Duck Number ");
+		} else System.out.println(n+" is not a Duck Number ");
+		if(isArmstrong(n)){
+			System.out.println(n+" is a ArmStrong Number ");
+		} else System.out.println(n+" is not a ArmStrong Number ");
 	}
 	static int count(int n){
 		int temp = n;
@@ -27,5 +33,18 @@ class NumberChecker1{
 		}
 		return sum;
 	}
+	   public static boolean isDuckNumber(int number) {
+        return String.valueOf(number).contains("0") && number > 0;
+    }
+
+    public static boolean isArmstrong(int number) {
+        int sum = 0, temp = number, digits = String.valueOf(number).length();
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, digits);
+            temp /= 10;
+        }
+        return sum == number;
+    }
 	
 }
